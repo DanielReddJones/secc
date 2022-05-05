db0f6a43caecd6c938beae0e0c633a87e79706e7
Daniel Jones <104509116+DanielReddJones@users.noreply.github.com>
2022-05-05 16:09:23 -0500

    Basic gui

--- secc_client.py
+++ secc_client.py
@@ -1,13 +1,25 @@
 import rsa
-import PyQt5
+import tkinter as tk
 import socket
 
 
 
 
 def main():
-    print("hello! Generating keys...")
-    pubkey, privatekey = genKeys()
+
+    #creating basic GUI elements
+    window = tk.Tk()
+    window.geometry("800x500")
+    recieverBox = tk.Text(window, height = 20, width = 80)
+    senderBox = tk.Text(window, height = 5, width = 80)
+    senderButt = tk.Button(text = "send")
+
+    #placing onto the window
+    senderButt.grid(row = 1, column = 1, pady = 10, padx = 10)
+    recieverBox.grid(row = 0, column = 0, pady = 10, padx = 1)
+    senderBox.grid(row = 1, column = 0, pady = 10, padx = 1)
+    window.mainloop()
+
 
 
 def genKeys():
--- secc_serv.py
+++ secc_serv.py
@@ -3,6 +3,7 @@
 #Language: Python 3.10.2
 #IDE: Atom
 #OS: Windows/PopOS
+#last modified: 5/13/2022
 ####################
 import threading
 import socket
@@ -14,18 +15,22 @@ nicknames = []
 
 #todo: Will need to set up way to get public-facing IP automatically to save to conf file.
 #Using localhost for testing.
-host = '127.0.0.1'
 
+
+host = '127.0.0.1'
 #9 for men 7 for dwarves, 3 for elves, and 1 secret
 port = 9731
+
+print("host is ", host, " port is ", port)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(host, port)
+server.bind((host, port))
 server.listen()
 
 #broadcasts message to every client.
 def broadcast(message):
     for client in clients:
         client.send(message)
+
 
 def handle(client):
     while True:
@@ -36,12 +41,30 @@ def handle(client):
             #if exception, removes IP and nickname from list.
             index = clients.index(client)
             clients.remove(client)
-
             nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat.'.encode('ascii'))
             nicknames.remove(nickname)
+            break
 
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"connected with {str(address)}")
 
+        #prompts client to ask for nickname and adds to list.
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        print(f'Nickname is {nickname}!')
+        broadcast(f'{nickname} has joined the chat.')
 
+        client.send("connected to the server.".encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client))
+        thread.start()
+
+receive()
 #reads configuration file.
 #config file contains IP address and preferred port. Default port is 9731, but is changeable if user wants something else.
 def getConf():
