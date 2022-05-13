1efa82e051babc8826dee464b4b3fbc83d740f95
Daniel Jones <104509116+DanielReddJones@users.noreply.github.com>
2022-05-13 11:53:15 -0500

    Added cryptography library for voice encryption

--- secc_client.py
+++ secc_client.py
@@ -1,6 +1,16 @@
-import rsa
-import tkinter as tk
+####################
+#Author: Daniel Jones
+#Language: Python 3.10.2
+#IDE: Atom
+#OS: Windows/PopOS
+#last modified: 5/13/2022
+####################
+
+
+import rsa #used for text encryption/decryption
+import tkinter as tk 
 import socket
+import cryptography #used for voice encryption/decryption
 
 
 
@@ -12,12 +22,14 @@ def main():
     window.geometry("800x500")
     recieverBox = tk.Text(window, height = 20, width = 80)
     senderBox = tk.Text(window, height = 5, width = 80)
+    voiceBox = tk.Button(text= "speak")
     senderButt = tk.Button(text = "send")
 
     #placing onto the window
     senderButt.grid(row = 1, column = 1, pady = 10, padx = 10)
     recieverBox.grid(row = 0, column = 0, pady = 10, padx = 1)
     senderBox.grid(row = 1, column = 0, pady = 10, padx = 1)
+    voiceBox.grid(row = 1, column = 2, pady = 10, padx = 10)
     window.mainloop()
 
 
